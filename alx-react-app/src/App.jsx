891acdae091809b90a1b67782bde11edbdf71ae9
Rackel Odiwuor <odiwuorrackel@gmail.com>
2024-08-10 10:43:34 +0300

    added a header,footer and maincontent files

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import {MainContent} from './components/MainContent'
+import {Footer} from './components/Footer'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -16,6 +19,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
         <WelcomeMessage />
+        
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -25,6 +29,9 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <Header />
+        <MainContent />
+        <Footer />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
