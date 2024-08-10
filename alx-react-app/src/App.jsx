1cf76c7d4e4c749c93ebc8e0f46b8d770789ae64
Rackel Odiwuor <odiwuorrackel@gmail.com>
2024-08-10 10:46:54 +0300

    removed te curly braces

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,8 +4,8 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
-import {MainContent} from './components/MainContent'
-import {Footer} from './components/Footer'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 function App() {
   const [count, setCount] = useState(0)
 
