4369c9f5def2caf5e3ec79e5cef8577f29fd939c
Rackel Odiwuor <odiwuorrackel@gmail.com>
2024-08-16 21:13:39 +0300

    updated styling

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header () {
     return(
-        <header style={{backgroundColor: 'black', color: 'white', textAlign: 'center'}}>
+        <header style={{backgroundColor: 'navy', color: 'white', textAlign: 'center'}}>
   <h1>My Favorite Cities</h1>
 </header>
     );
--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile () {
     return (
-        <div style={{border: '2px solid grey', padding: '10px', margin: '10px'}}>
-          <h2 style={{color: 'black'}}>{props.name}</h2>
+        <div style={{border: '1px solid grey', padding: '10px', margin: '10px'}}>
+          <h2 style={{color: 'blue'}}>{props.name}</h2>
           <p>Age:<span style= {{fontWeight:'bold',fontFamily:'sans-serif'}}> {props.age}</span></p>
           <p>Bio: {props.bio}</p>
         </div>
