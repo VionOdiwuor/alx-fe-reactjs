e410317947d23258bb88b1035de34331bb401a1a
Rackel Odiwuor <odiwuorrackel@gmail.com>
2024-08-16 21:30:04 +0300

    added a new file in the components folder

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -32,6 +33,7 @@ function App() {
         </p>
         <Header />
         <MainContent />
+        <Counter />
         <Footer />
       </div>
       <p className="read-the-docs">
