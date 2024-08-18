1b2d280f2c7c7f03991c6eff024f6402d278ed1c
Rackel Odiwuor <odiwuorrackel@gmail.com>
2024-08-18 11:13:54 +0300

    added a useContext hook

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,9 +1,14 @@
 function UserProfile () {
+  const user = {
+    name: "John Doe",
+    age: 30,
+    bio: "Software Engineer"
+  };
     return (
         <div style={{border: '1px solid grey', padding: '10px', margin: '10px'}}>
-          <h2 style={{color: 'blue'}}>{props.name}</h2>
-          <p>Age:<span style= {{fontWeight:'bold',fontFamily:'sans-serif'}}> {props.age}</span></p>
-          <p>Bio: {props.bio}</p>
+          <h2 style={{color: 'blue'}}>{props.user.name}</h2>
+          <p>Age:<span style= {{fontWeight:'bold',fontFamily:'sans-serif'}}> {props.user.age}</span></p>
+          <p>Bio: {props.user.bio}</p>
         </div>
       );
     };
---
+++ alx-react-app-props/src/components/UserInfo.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import UserDetails from './UserDetails';
+
+function UserInfo() {
+  return (
+    <div>
+      <h1>Profile page</h1>
+      <UserDetails />
+    </div>
+ 
+)
+}
+
+export default UserInfo;
