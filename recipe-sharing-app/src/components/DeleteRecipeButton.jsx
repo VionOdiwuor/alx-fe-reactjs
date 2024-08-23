bf4d506dd330596dbe48650717bacede3081f390
Rackel Odiwuor <odiwuorrackel@gmail.com>
2024-08-23 18:27:20 +0300

    added a navigate hook

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useRecipeStore } from '../store/recipeStore';
 
 const DeleteRecipeButton = ({ id }) => {
-  const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
+    const navigate = useNavigate(); 
+    const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
 
   const handleDelete = () => {
     if (window.confirm('Are you sure you want to delete this recipe?')) {
       deleteRecipe(id);
+      navigate('/RecipeList'); // Redirect to home or any other page after deletion
     }
   };
 
